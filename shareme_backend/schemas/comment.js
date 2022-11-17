43bd9ad1eae503f9b3eec330f2dcf8079c4564db
Borislav Stefanov <prohaies3101@gmail.com>
2022-11-17 15:23:01 +0200

    backendForShareMe

---
+++ shareme_backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+    name:"comment",
+    title:"Comment",
+    type:"document",
+    fields: [
+        {
+            name:"prostedBy",
+            title:"PostedBy",
+            type:"postedBy"
+        },
+        {
+            name:"comment",
+            title:"Comment",
+            type:"string"
+        },
+    ]
+}
