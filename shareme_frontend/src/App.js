c26cfc71917af226213f5743ca8ba84233eb7a70
Borislav Stefanov <prohaies3101@gmail.com>
2022-11-17 16:07:11 +0200

    frontendForShareMe

---
+++ shareme_frontend/src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
+import { Login } from './components';
+import Home from './container/Home';
+import { gapi } from "gapi-script";
+
+const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+    if (!User) navigate('/login');
+  }, []);
+
+  gapi.load("client:auth2", () => {
+    gapi.client.init({
+      clientId:
+        "*****.apps.googleusercontent.com",
+      plugin_name: "chat",
+    });
+  });
+  return (
+<Routes>
+  <Route path="login" element={<Login />} />
+  <Route path="/*" element={<Home />} />
+</Routes>
+  )
+}
+
+export default App
